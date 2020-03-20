5896b26ae970549a25bb24e7e60b722e6182784a
Amy Oulton <amyeoulton@gmail.com>
2020-03-19 21:03:43 -0700

    finished box-it

--- box_it_script/box_it_script.js
+++ box_it_script/box_it_script.js
@@ -8,3 +8,22 @@ const drawBottomBorder = (n) => "└" + drawLine(n) + "┘"
 // Draw Bars Around Function
 
 const drawBarsAround = (string) => "|" + string + "|"
+
+// Putting it all together
+
+const boxIt = (arr) => {
+    const newLines = '\n'
+    let l = Math.max(...arr.map(a => a.length))
+    let output = ''
+    for(let i = 0; i < arr.length; i++){
+      word = arr[i] + ' '.repeat(l - arr[i].length)
+      if(i === arr.length - 1){
+        output += newLines + drawBarsAround(word) + newLines
+      } else output += newLines + drawBarsAround(word) + newLines + drawMiddleBorder(l)
+     }
+return drawTopBorder(l) + output + drawBottomBorder(l)
+}
+
+let array = process.argv.slice(2);
+
+console.log(boxIt(array))
