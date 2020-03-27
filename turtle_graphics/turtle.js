2150f426a60b040f4b4932c19efef1c2053f5a6e
Amy Oulton <amyeoulton@gmail.com>
2020-03-26 19:04:52 -0700

    delete bad turtle, updated right prototype

--- turtle_graphics/turtle.js
+++ turtle_graphics/turtle.js
@@ -1,69 +1,27 @@
 class Turtle {
     
     constructor(x, y){
-        const turtle = '*'
-        let turtleLog = []
-        let position;
-        this.x = x;
-        this.y = y;
-    }
-    forward(x){
-        this.x += x
-        if(position === 'right'){
-            this.y += 1
-        }
-        if(position === 'left'){
-            this.y -= 1
-        }
-        this.allPoints(this.x, this.y)
-    }
-
-    right(){
-        position = 'right'
-    }
-
-    left(){
-        position = 'left'
-    }
-
-    allPoints(x, y){
-        turtleLog.push(x, y)
-    }
-
-    print(){
-        for(log of turtleLog){
-            while(log[0] !== log[2]){
-
-            }
-        }
-        }
-    }
-
-// everything below this point is closer than above but also completely wrong
-
-class Turtle {
-    
-    constructor(x, y){
         this.turtle = '*'
         this.turtleLog = [[x, y]]
-        this.position = ''
         this.x = x;
         this.y = y;
+        this.direction = 'east'
     }
+    
     forward(x){
-        if(this.position === 'right'){
-            this.y += x
-        } else if(this.position === 'left'){
-            this.y -= x
-        }
-        for(let i = 0; i <= x; i++){
-          this.y += i
-          this.allPoints(i, this.y)
     }
     }
 
     right(){
-        this.position = 'right'
+        if(this.direction === 'east'){
+            this.direction = 'south'
+        } else if(this.direction === 'south'){
+            this.direction = 'west'
+        } else if(this.direction === 'west'){
+            this.direction = 'north'
+        } else if(this.direction === 'north'){
+            this.direction = 'east'
+        }
     }
 
     left(){
