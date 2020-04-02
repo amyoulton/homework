07ca3391a8c9fcbaf998a6cd8558a750a277e668
Amy Oulton <amyeoulton@gmail.com>
2020-04-01 19:36:15 -0700

    added view and new

--- to_do_cli/to_do.js
+++ to_do_cli/to_do.js
@@ -1,12 +1,63 @@
-const fs = require('fs')
-
-const readline = require("readline");
+const fs = require('fs');
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const menu = "\nWelcome to ToDo CLI!\n \n-------------------\n\n(v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit";
+// variables for the menu
+const question = '\nWelcome to ToDo CLI!\n \n-------------------\n';
+const menu =
+  '\n(v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit\n\n';
 
-rl.write(menu)
+//variables for creating new items
+let num = 0;
+const box = '☐';
+const checkBox = '☑';
+
+//variables for completing items
+let done;
+let array = [];
+let line = '';
+
+const application = () => {
+  rl.question(menu, answer => {
+    if (answer === 'v') {
+      fs.readFile('./to_do_list.txt', (err, data) => {
+        if (err) throw err;
+        console.log('\n' + data.toString());
+        return application();
+      });
+    } else if (answer === 'n') {
+      rl.question('\nWhat would you like to add?\n\n', answer => {
+        num += 1;
+        fs.appendFile(
+          './to_do_list.txt',
+          `${num} ${box} ${answer}\n\n`,
+          'utf8',
+          err => {
+            if (err) throw err;
+            return application();
+          }
+        );
+      });
+    } else if (answer.includes('c')) {
+      done = answer.substring(1);
+      fs.readFile('./to_do_list.txt', 'utf8', function(err, data) {
+        if (err) {
+          return console.log(err);
+        }
+        arr = data.split('\n');
+        arr.forEach();
+        console.log(arr);
+        // fs.writeFile(someFile, result, 'utf8', function(err) {
+        //   if (err) return console.log(err);
+        // });
+      });
+    }
+  });
+};
+
+rl.write(question);
+rl.write(application());
