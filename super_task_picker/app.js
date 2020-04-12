4518c2011485fb50789039bf9af3559cc9530a7a
Amy Oulton <amyeoulton@gmail.com>
2020-04-12 12:54:11 -0700

    set up app.js, set up baseRouter.js

---
+++ super_task_picker/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(
+  methodOverride((request, response) => {
+    if (request.body && request.body._method) {
+      const method = request.body._method;
+      return method;
+    }
+  })
+);
+
+const baseRouter = require('./routes/baseRouter');
+app.use('/', baseRouter);
+
+const PORT = 3000;
+const ADDRESS = 'localhost'; // 127.0.0.1
+app.listen(PORT, ADDRESS, () => {
+  console.log(`Server listening on http://${ADDRESS}:${PORT}`);
+});
